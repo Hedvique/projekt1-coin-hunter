45c4ea637c22f9a531282bf11d2affdb1c059ea1
Simona Brabcová <simona.brabcova@email.cz>
2022-02-26 19:33:16 +0100

    rozhýbání panáčka pomocí šipek

--- main.js
+++ main.js
@@ -8,10 +8,29 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 
 // sem začni psát svůj program
 
-let panacek = document.getElementById("panacek")
-let startovniPozice =
-	panacek.style.top = "50%"; 
-	panacek.style.left = "50%";
+let panacek = document.querySelector("img")
 
+
+let startovniPozice = null
+
+function test(event) {
+	if (event.key === "ArrowDown") {
+		panacek.src = '/obrazky/panacek.png'
+		startovniPozice++;
+		panacek.style.top = startovniPozice + 'px';
+	} else if (event.key === "ArrowLeft") {
+		panacek.src = '/obrazky/panacek-vlevo.png'
+		startovniPozice--;
+		panacek.style.left = startovniPozice + 'px';
+	} else if (event.key === "ArrowUp") {
+		panacek.src = '/obrazky/panacek-nahoru.png'
+		startovniPozice--;
+		panacek.style.top = startovniPozice + 'px';
+	} else if(event.key === "ArrowRight") {
+		panacek.src = '/obrazky/panacek-vpravo.png'
+		startovniPozice++;
+		panacek.style.left = startovniPozice + 'px';
+	}
+}
 
 
