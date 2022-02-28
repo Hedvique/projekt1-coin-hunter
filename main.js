24503921caed457a96607eef0984b80e98588cf3
Simona Brabcová <simona.brabcova@email.cz>
2022-02-28 23:44:26 +0100

    aktualizace pohybu panáčka

--- main.js
+++ main.js
@@ -10,27 +10,32 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 
 let panacek = document.querySelector("img")
 
+panacek.style.top = parseInt(window.innerHeight / 2) + 'px';
+panacek.style.left = parseInt(window.innerWidth / 2) + 'px';
 
-let osaX = null;
-let osaY = null;
+let osaX = panacek.style.left;
+let osaY = panacek.style.top;
+
+
+let pohyb = null
 
 function pohybPanacka(event) {
-	if (event.key === "ArrowDown") {
-		panacek.src = '/obrazky/panacek.png'
-		osaY++;
-		panacek.style.top = osaY + 'px';
-	} else if (event.key === "ArrowUp") {
-		panacek.src = '/obrazky/panacek-nahoru.png'
-		osaY--;
-		panacek.style.top = osaY + 'px';
-	} else if (event.key === "ArrowLeft") {
-		panacek.src = '/obrazky/panacek-vlevo.png'
-		osaX--;
-		panacek.style.left = osaX + 'px';
-	}else if(event.key === "ArrowRight") {
-		panacek.src = '/obrazky/panacek-vpravo.png'
-		osaX++;
-		panacek.style.left = osaX + 'px';
+	if (event.key === "ArrowDown" && (parseInt(osaY) + pohyb) <= window.innerHeight) {
+		panacek.src = '/obrazky/panacek.png';
+		pohyb += 10;
+		panacek.style.top = parseInt(osaY) + pohyb + "px";
+	} else if (event.key === "ArrowUp" && (parseInt(osaY) - pohyb) <= window.innerHeight) {
+		panacek.src = '/obrazky/panacek-nahoru.png';
+		pohyb -= 10;
+		panacek.style.top = parseInt(osaY) + pohyb + "px";
+	} else if (event.key === "ArrowLeft" && (parseInt(osaX) - pohyb) <= window.innerWidth) {
+		panacek.src = '/obrazky/panacek-vlevo.png';
+		pohyb -= 10;
+		panacek.style.left = parseInt(osaX) + pohyb + "px";
+	} else if (event.key === "ArrowRight" && (parseInt(osaX) + pohyb) <= window.innerWidth) {
+		panacek.src = '/obrazky/panacek-vpravo.png';
+		pohyb += 10;
+		panacek.style.left = parseInt(osaX) + pohyb + "px"
 	}
 }
 
